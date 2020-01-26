266d2fd16338a3b095e8adc85ee3331a6282fb59
Nolan Wright <nolan@nolanwright.dev>
2020-01-26 15:09:09 -0500

    Improve solution to problem 7

--- problems/problem07.py
+++ problems/problem07.py
@@ -13,26 +13,26 @@ determine if the input string is valid.
 [1]: https://leetcode.com/problems/valid-parentheses/
 '''
 
+
 def solution(s):
 	'''
-	Ensure the first character is an opening delimeter. Find the closing delimiter
-	which corresponds to this first character. It may not be the very next closing
-	delimiter of its sort. Once the match is found, recurse on the parts of the
-	string which exist within the matching delimiters and to the right of them.
+	Push opening delimiters onto a stack while iterating through the input string.
+	When a closing delimiter is encountered, pop a delimiter off the stack and
+	ensure it matches the closing one. The input string is valid if the stack is
+	empty after completely iterating through the input string.
 	'''
 	if not s:
 		return True
-	if len(s) % 2 or s[0] not in '({[':
+	if len(s) % 2 or s[0] in ')}]' or s[-1] in '({[':
 		return False
-	count = 0
-	closing = {'(': ')', '{': '}', '[': ']'}[s[0]]
-	for i, delimiter in enumerate(s[1:], 1):
-		if delimiter == s[0]:
-			count += 1
-		if delimiter == closing:
-			if count == 0:
-				return solution(s[1:i]) and (i == len(s) - 1 or solution(s[i+1:]))
-			count -= 1
-			if c < 0:
-				break
-	return False
+	stack = []
+	for delimiter in s:
+		if delimiter in '({[':
+			stack.append(delimiter)
+		elif not stack or stack.pop() != {
+		    ')': '(',
+		    '}': '{',
+		    ']': '['
+		}[delimiter]:
+			return False
+	return not stack
